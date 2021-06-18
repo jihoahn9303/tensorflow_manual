4cd7a525f7cf6074c1b1d822c79c762669f7e414
jihoahn9303 <rediate03@gmail.com>
2021-06-18 22:29:52 +0900

    upload files handling how to load the dataset

---
+++ code/load_dataset.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 17:30:01 2021
+
+@author: jiho Ahn
+@topic: load dataset with tensorflow(1)
+"""
+import tensorflow as tf
+import numpy as np
+
+train_x = np.arange(500).astype(np.float32).reshape(-1, 1)
+train_y = 3*train_x + 1
+
+# np.ndarray or list -> tensor dataset object
+train_ds = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+train_ds = train_ds.shuffle(100).batch(32)
+
+for x, y in train_ds:
+    print(x.shape, y.shape, '\n')
+
+# %%
+from tensorflow.keras.datasets import mnist
+import matplotlib.pyplot as plt
+
+(train_images, train_labels),(test_images, test_labels) = mnist.load_data()
+'''
+print(type(train_images))  # numpy.ndarray
+print(train_images.shape)
+print(train_labels.shape)
+print(test_images.shape)
+print(test_labels.shape)
+'''
+
+train_ds = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+train_ds = train_ds.shuffle(100).batch(9)
+
+test_ds = tf.data.Dataset.from_tensor_slices((test_images, test_labels))
+test_ds = test_ds.batch(32)
+
+'''
+train_ds_iter = iter(train_ds)
+images, labels = next(train_ds_iter)
+
+fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(10,10), squeeze=False)
+
+for ax_idx, ax in enumerate(axes.flat):
+    image = images[ax_idx, ...]
+    label = labels[ax_idx]
+    
+    ax.imshow(image.numpy(), 'gray')
+    ax.set_title(label.numpy(), fontsize=20)
+'''
+    
+
---
+++ code/load_dataset2.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 17 18:12:18 2021
+
+@author: jiho Ahn
+@topic: load dataset with tensorflow(2)
+"""
+
+import tensorflow_datasets as tfds
+
+# load dataset with additional information
+
+#dataset, ds_info = tfds.load(name='mnist', 
+#                             shuffle_files=True, 
+#                             with_info=True)
+
+'''
+print(ds_info)
+print(ds_info.features)
+print(ds_info.splits)
+'''
+
+# %%
+'''
+dataset = tfds.load(name='mnist', 
+                    shuffle_files=True)
+
+train_ds = dataset['train'].batch(32)
+test_ds = dataset['test'] 
+
+for tmp in train_ds:
+    
+    print(type(tmp))
+    print(tmp.keys())
+    
+    images = tmp['image']
+    labels = tmp['label']
+    
+    print(images.shape)
+    print(labels.shape)
+    break
+    
+'''
+
+# %%
+# auto-unpack dataset to images and labels
+'''
+dataset = tfds.load(name='mnist',
+                    shuffle_files=True,
+                    as_supervised=True)
+
+train_ds = dataset['train'].batch(32)
+test_ds = dataset['test']
+
+
+for tmp in train_ds:
+    images = tmp[0]
+    labels = tmp[1]
+    
+    print(images.shape)
+    print(labels.shape)
+    
+    break
+
+
+for images, labels in train_ds:
+    print(images.shape)
+    print(labels.shape)
+    
+    break
+'''
+
+# %%
+# auto-split data(1)
+'''
+(train_ds, test_ds), ds_info = tfds.load(name='mnist',
+                                         shuffle_files=True,
+                                         as_supervised=True,
+                                         split=['train', 'test'],
+                                         with_info=True)
+
+train_ds = train_ds.batch(32)
+
+for images, labels in train_ds:
+    print(images.shape)
+    print(labels.shape)
+    break
+'''    
+# %%
+# auto-split data(2)
+(train_ds, validation_ds, test_ds), ds_info = tfds.load(name='patch_camelyon',
+                                                        shuffle_files=True,
+                                                        as_supervised=True,
+                                                        split=['train', 'validation', 'test'], 
+                                                        with_info=True,
+                                                        batch_size=9)
+
+# train_ds = train_ds.batch(9)
+train_ds_iter = iter(train_ds)
+images, labels = next(train_ds_iter)
+
+import matplotlib.pyplot as plt
+    
+fig, axes = plt.subplots(nrows=3, ncols=3, figsize=(15,15), squeeze=False)
+
+for ax_idx, ax in enumerate(axes.flat):
+    ax.imshow(images[ax_idx, ...].numpy())
+    ax.set_title(labels[ax_idx].numpy(), fontsize=30)
+    
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
